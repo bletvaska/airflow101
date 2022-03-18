127b78c7fc624e799e9aba4e625cdd421462687a
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-03-18 13:04:52 +0100

    python operator

--- dags/weather_scraper.py
+++ dags/weather_scraper.py
@@ -3,14 +3,19 @@ from datetime import datetime
 from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.providers.http.sensors.http import HttpSensor
+
+
+def _filter_data():
+    print('Here we are' * 20)
+
 
 with DAG('openweathermap_scraper',
          description='Scrapes the weather data from Openweathermap.org',
          schedule_interval='*/15 * * * *',
          start_date=datetime(2022, 3, 18)) as dag:
-    task1 = DummyOperator(task_id='scrape_data')
     task2 = DummyOperator(task_id='process_data')
     task3 = DummyOperator(task_id='store_data')
 
@@ -34,10 +39,14 @@ with DAG('openweathermap_scraper',
         request_params={
             'appid': Variable.get('openweathermap_appid')
         },
-        extra_options={'check_response': False},
         poke_interval=10,
-        timeout=30
-        # retries=3
+        timeout=30,
+        method='GET',
     )
 
-    service_availability >> scrape_data >> task1 >> task2 >> task3
+    data_preprocessor = PythonOperator(
+        task_id='preprocess_data',
+        python_callable=_filter_data
+    )
+
+    service_availability >> scrape_data >> data_preprocessor >> task2 >> task3

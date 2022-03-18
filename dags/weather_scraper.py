81faa72747766967ff025bcb3ae050cbf2e25c74
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-03-18 11:09:05 +0100

    HttpSensor

--- dags/weather_scraper.py
+++ dags/weather_scraper.py
@@ -4,6 +4,7 @@ from airflow import DAG
 
 from airflow.operators.dummy import DummyOperator
 from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.providers.http.sensors.http import HttpSensor
 
 with DAG('openweathermap_scraper',
          description='Scrapes the weather data from Openweathermap.org',
@@ -25,4 +26,17 @@ with DAG('openweathermap_scraper',
         }
     )
 
-    scrape_data >> task1 >> task2 >> task3
+    service_availability = HttpSensor(
+        task_id='is_weather_api_available',
+        http_conn_id='openweathermap_api',
+        endpoint='/data/2.5/weather',
+        request_params={
+            'appid': '08f5d8fd385c443eeff6608c643e0bc5'
+        },
+        extra_options={'check_response': False},
+        poke_interval=10,
+        timeout=30
+        # retries=3
+    )
+
+    service_availability >> scrape_data >> task1 >> task2 >> task3

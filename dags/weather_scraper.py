9b05d541b0e8fb3e573bf4effdc23a7da7775266
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-09-30 13:15:44 +0200

    created validators for model

--- dags/weather_scraper.py
+++ dags/weather_scraper.py
@@ -9,7 +9,7 @@ from airflow.hooks.base import BaseHook
 from airflow.models import Variable
 import requests
 import jsonschema
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 CONNECTION_ID = 'openweathermap'
 
@@ -20,6 +20,32 @@ class Measurement(BaseModel):
     city: str
     country: str
     dt: datetime
+
+    @validator('humidity')
+    def humidity_must_be_in_range_from_zero_to_hundred(cls, value):
+        if 0 <= value <= 100:
+            return value
+
+        raise ValueError('must be in range [0, 100]')
+
+    @validator('temperature')
+    def temperature_must_be_greater_than_absolute_zero(cls, value):
+        if value < -273:
+            raise ValueError('must be greater than -273')
+
+        return value
+
+    @validator('country')
+    def coutry_must_be_specific(cls, value):
+        if len(value) != 2:
+            raise ValueError('length of country code must be 2')
+
+        elif value not in ['SK', 'CZ', 'HU', 'PL', 'UA', 'AU']:
+            raise ValueError('country must be one of specific')
+
+        else:
+            return value
+
 
 
 with DAG("weather_scraper",

b76adf2c5c9e8eb403032ad6a933d3e2c94122e7
mirek <mirek@cnl.sk>
2023-05-17 13:24:34 +0200

    refactoring

--- dags/weather_scraper.py
+++ dags/weather_scraper.py
@@ -13,6 +13,8 @@ from airflow.hooks.base import BaseHook
 import boto3
 import botocore
 
+from helper import is_minio_alive
+
 
 # create logger
 logger = logging.getLogger(__name__)  # weather_scraper
@@ -45,16 +47,6 @@ with DAG(
             logger.critical("Invalid API key.")
             raise AirflowFailException("Invalid API key.")
 
-    @task
-    def is_minio_alive():
-        logger.info("is minio alive")
-
-        base_url = BaseHook.get_connection("minio").host
-        response = httpx.head(f"{base_url}/minio/health/live")
-
-        if response.status_code != 200:
-            logger.critical("Minio is not Alive")
-            raise AirflowFailException("MinIo is not Alive.")
 
     @task
     def scrape_data() -> dict:

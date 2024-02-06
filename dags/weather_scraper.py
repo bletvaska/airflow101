f99499db0d3d6b85b779015c54803d5f516ac73a
mirek <mirek@cnl.sk>
2024-02-06 09:27:59 +0100

    reformatovany kod

--- dags/weather_scraper.py
+++ dags/weather_scraper.py
@@ -9,17 +9,14 @@ from pendulum import datetime
 
 logger = logging.getLogger(__file__)
 
+
 @task
 def scrape_data(query: str) -> dict:
-    logger.info('Scraping data')
+    logger.info("Scraping data")
 
-    connection = BaseHook.get_connection('openweathermap')
+    connection = BaseHook.get_connection("openweathermap")
 
-    params = {
-        'q': query,
-        'units': 'metric',
-        'appid': connection.get_password()
-    }
+    params = {"q": query, "units": "metric", "appid": connection.get_password()}
 
     response = httpx.get(connection.host, params=params)
     return response.json()
@@ -27,19 +24,19 @@ def scrape_data(query: str) -> dict:
 
 @task
 def process_data(data: dict) -> str:
-    logger.info('Processing data')
+    logger.info("Processing data")
 
-    line = '{},{},{},{},{},{},{},{},{},{}'.format(
-        data['dt'],
-        data['name'],
-        data['sys']['country'],
-        data['main']['temp'],
-        data['main']['humidity'],
-        data['main']['pressure'],
-        data['sys']['sunrise'],
-        data['sys']['sunset'],
-        data['wind']['deg'],
-        data['wind']['speed'],
+    line = "{},{},{},{},{},{},{},{},{},{}".format(
+        data["dt"],
+        data["name"],
+        data["sys"]["country"],
+        data["main"]["temp"],
+        data["main"]["humidity"],
+        data["main"]["pressure"],
+        data["sys"]["sunrise"],
+        data["sys"]["sunset"],
+        data["wind"]["deg"],
+        data["wind"]["speed"],
     )
 
     return line
@@ -47,39 +44,45 @@ def process_data(data: dict) -> str:
 
 @task
 def publish_data(line: str):
-    logger.info('Publishing data')
+    logger.info("Publishing data")
 
-    with open('/home/ubuntu/dataset.csv', 'a') as dataset:
+    with open("/home/ubuntu/dataset.csv", "a") as dataset:
         print(line, file=dataset)
 
 
 @task
 def is_service_alive():
     try:
-        connection = BaseHook.get_connection('openweathermap')
-        params = {
-            'appid': connection.get_password()
-        }
+        connection = BaseHook.get_connection("openweathermap")
+        params = {"appid": connection.get_password()}
         response = httpx.get(connection.host, params=params)
         if response.status_code == 401:
-            raise AirflowFailException('Invalid API Key')
+            raise AirflowFailException("Invalid API Key")
     except httpx.ConnectError:
-        logger.error(f'Invalid hostname {connection.host}')
-        raise AirflowFailException('Invalid host name.')
+        logger.error(f"Invalid hostname {connection.host}")
+        raise AirflowFailException("Invalid host name.")
 
 
 @dag(
-    'weather_scraper',
-    description='Scrapes weather from openweathermap.org',
-    schedule='*/20 * * * *',
-    start_date=datetime(2024, 1, 1, tz='UTC'),
-    tags=['weather', 'devops', 'telekom'],
-    catchup=False
+    "weather_scraper",
+    description="Scrapes weather from openweathermap.org",
+    schedule="*/20 * * * *",
+    start_date=datetime(2024, 1, 1, tz="UTC"),
+    tags=["weather", "devops", "telekom"],
+    catchup=False,
 )
-def main(query=Param(type='string', default='kosice,sk', title='Query', description='Name of the city to get weather info about')):
+def main(
+    query=Param(
+        type="string",
+        default="kosice,sk",
+        title="Query",
+        description="Name of the city to get weather info about",
+    ),
+):
     # is_service_alive | scrape_data | process_data | publish_data
-    data = is_service_alive() >> scrape_data('kosice,sk')
+    data = is_service_alive() >> scrape_data("kosice,sk")
     processed_data = process_data(data)
     publish_data(processed_data)
+
 
 main()

714678e5000b641fe44990b51f6779fb4bc31791
mirek <mirek@cnl.sk>
2024-04-26 15:12:09 +0200

    healthcheck serveru openweathermap.org pomocou modulu sh

--- dags/weather_scraper.py
+++ dags/weather_scraper.py
@@ -2,12 +2,18 @@ import json
 from pathlib import Path
 import sys
 from http import HTTPStatus
+import logging
 
 from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
+from airflow.exceptions import AirflowFailException
 import httpx
 from pendulum import datetime
 import jsonschema
+from sh import ping
+
+
+logger = logging.getLogger(__name__)
 
 
 @task
@@ -15,16 +21,17 @@ def scrape_data(query):
     """
     Scrapes the data from openweathermap.org
     """
-    print(">> Scraping Data")
+    logger.info(">> Scraping Data")
 
     conn = BaseHook.get_connection("openweathermap")
 
     url = f"{conn.schema}://{conn.host}/data/2.5/weather"
-    params = {"q": query, "appid": conn.password, "units": 'metric'}
+    params = {"q": query, "appid": conn.password, "units": "metric"}
     response = httpx.get(url, params=params)
 
     if response.status_code == HTTPStatus.NOT_FOUND:
-        sys.exit("Error: City not found.")
+        raise AirflowFailException('City not found.')
+        # sys.exit("Error: City not found.")
 
     if response.status_code == HTTPStatus.UNAUTHORIZED:
         sys.exit("Error: Invalid API Key.")
@@ -38,7 +45,7 @@ def process_data(data):
     """
     Process the passed data
     """
-    print(">> Processing Data")
+    logger.info(">> Processing Data")
 
     return "{},{},{},{},{},{},{}".format(
         data["dt"],
@@ -56,21 +63,27 @@ def publish_data(line):
     """
     Publish/persist the data to CSV file
     """
-    print(">> Publishing Data")
-    path = Path(__file__).parent / 'dataset.csv'
+    logger.info(">> Publishing Data")
+    
+    path = Path(__file__).parent / "dataset.csv"
 
     with open(path, "a") as file:
         print(line, file=file)
-        
-        
+
+
 @task
 def validate_data(data: dict):
-    path = Path(__file__).parent / 'weather.schema.json'
-    
-    with open(path, 'r') as file:
+    path = Path(__file__).parent / "weather.schema.json"
+
+    with open(path, "r") as file:
         schema = json.load(file)
         jsonschema.validate(data, schema)
         return data
+    
+@task
+def healthcheck_weather():
+    conn = BaseHook.get_connection('openweathermap')
+    ping(conn.host, '-c', '1', _timeout=3)
 
 
 @dag(
@@ -82,10 +95,10 @@ def validate_data(data: dict):
     catchup=False,
 )
 def main(query: str = "kosice"):
-    measurement = scrape_data(query)
+    measurement = healthcheck_weather() >> scrape_data(query)
     valid_data = validate_data(measurement)
     line = process_data(valid_data)
     publish_data(line)
-
+    
 
 main()

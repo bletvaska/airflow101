dc74d48f5b30baedcc185a2fc29909c421e1aeef
mirek <mirek@cnl.sk>
2024-05-03 11:36:22 +0200

    vytvorena uloha extract_yesterday_data()

--- dags/daily_report.py
+++ dags/daily_report.py
@@ -1,6 +1,14 @@
 import logging
+from pathlib import Path
+import tempfile
 from pendulum import datetime
 from airflow.decorators import dag, task
+from airflow.hooks.base import BaseHook
+import boto3
+from botocore.exceptions import ClientError
+from airflow.exceptions import AirflowFailException
+import pandas as pd
+import pendulum
 
 from tasks import healthcheck_minio
 
@@ -15,7 +23,50 @@ def create_report():
 
 @task
 def extract_yesterday_data():
-    pass
+    # minio client
+    conn = BaseHook.get_connection("minio")
+    minio = boto3.resource(
+        "s3",
+        endpoint_url=f"{conn.schema}://{conn.host}:{conn.port}",
+        aws_access_key_id=conn.login,
+        aws_secret_access_key=conn.password,
+    )
+
+    bucket = minio.Bucket("datasets")
+
+    # create temporary file
+    path = Path(tempfile.mkstemp()[1])
+
+    # download dataset
+    try:
+        bucket.download_file("dataset.csv", path)
+    except ClientError:
+        logger.warning("Dataset not found. Possibly first run.")
+        raise AirflowFailException("Dataset not found.")
+
+    # read and clean dataset
+    df = pd.read_csv(
+        path,
+        names=["dt", "city", "temp", "press", "hum", "wind_speed", "wind_deg"],
+        sep=",",
+    )
+    
+    # remove temporary file
+    path.unlink(True)
+    
+    # cleanup and normalize dataframe
+    df.drop_duplicates(inplace=True)
+    df['dt'] = pd.to_datetime(df['dt'], unit='s')
+    
+    # create filters
+    f_till_today = df['dt'] < pendulum.today('utc').naive()
+    f_since_yesterday = df['dt'] >= pendulum.yesterday('utc').naive()
+    filter_yesterday = f_till_today & f_since_yesterday
+    
+    # filter data
+    df = df.loc[ filter_yesterday, : ]
+    
+    logger.info(df)
 
 
 @dag(

397efa18963186e9a68a95e9971cb3159696c7e3
mirek <mirek@cnl.sk>
2024-05-09 12:44:23 +0200

    opraveny daily report

--- dags/daily_report.py
+++ dags/daily_report.py
@@ -22,42 +22,35 @@ def create_report(df: pd.DataFrame, ti: TaskInstance):
     # df = pd.read_json(data)
     # logger.info(df)
     # df.to_csv('/home/ubuntu/yesterday.csv')
-    
+
     # reset index
     df.index = range(0, len(df))
-    
 
     # prepare model
-    exec_date = pendulum.instance(ti.execution_date).start_of('day')
+    exec_date = pendulum.instance(ti.execution_date).start_of("day")
+    
     # from IPython import embed; embed()
     model = {
-        'city': df['city'][0],
-        'date': exec_date,
-        'max_temp': df['temp'].max(),
-        'min_temp': df['temp'].min(),
-        'avg_temp': df['temp'].mean(),
-        'temp_unit': '°C',
-        'timestamp': pendulum.now('utc').to_iso8601_string()
+        "city": df["city"][0],
+        "date": exec_date.to_date_string(),
+        "max_temp": df["temp"].max(),
+        "min_temp": df["temp"].min(),
+        "avg_temp": df["temp"].mean(),
+        # "temp_unit": "°C",
+        "timestamp": pendulum.now("utc").to_iso8601_string(),
     }
-    
+
     # prepare jinja2 environment
-    tpl_path = Path(__file__).parent / 'templates'
-    env = jinja2.Environment(
-        loader=jinja2.FileSystemLoader(tpl_path),
-        autoescape=False
-    )
-    
+    tpl_path = Path(__file__).parent / "templates"
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(tpl_path), autoescape=False)
+
     # get template
-    template = env.get_template('weather.tpl.j2')
-    
+    template = env.get_template("weather.tpl.j2")
+
     print(template.render(model))
     # tmp_path = Path(tempfile.mkstemp()[1])
     # with open(tmp_path, 'w') as file:
     #     print(template.render(model), file=file)
-    
-    
-    
-
 
 
 @task
@@ -90,17 +83,19 @@ def extract_yesterday_data(ti: TaskInstance) -> pd.DataFrame:
     df["dt"] = pd.to_datetime(df["dt"], unit="s")
 
     # create filters
-    exec_date = pendulum.instance(ti.execution_date).start_of('day')
+    exec_date = pendulum.instance(ti.execution_date).start_of("day")
     f_since_yesterday = df["dt"] >= exec_date.add(days=-1).naive()
     f_till_today = df["dt"] < exec_date.naive()
     filter_yesterday = f_till_today & f_since_yesterday
 
     # filter data
     df = df.loc[filter_yesterday, :]
-    
+
     # check if resulting dataframe is not empty
     if len(df) == 0:
-        raise AirflowFailException(f'No data to create report for date {exec_date.add(days=-1)}')
+        raise AirflowFailException(
+            f"No data to create report for date {exec_date.add(days=-1)}"
+        )
 
     # return df.to_json(date_format="iso")
     return df
@@ -120,3 +115,7 @@ def main():
 
 
 main()
+
+# https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/debug.html
+if __name__ == "__main__":
+    main().test()

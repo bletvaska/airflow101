68c28b83eb0bb68b6f123ed9ac85fb5fc53fd382
mirek <mirek@cnl.sk>
2024-10-02 13:44:41 +0200

    added notification task over apprise

--- dags/daily_report.py
+++ dags/daily_report.py
@@ -4,6 +4,8 @@ import tempfile
 
 from airflow.decorators import dag, task
 from airflow.exceptions import AirflowFailException
+from airflow.models import Variable
+from apprise import Apprise
 import pandas as pd
 from pendulum import datetime
 import botocore
@@ -46,19 +48,21 @@ def extract_yesterday_data(logical_date: pendulum.DateTime) -> DataFrame:
                 "wind_dir",
             ],
         )
-        
+
         # editing dataset
         df["dt"] = pd.to_datetime(df["dt"], unit="s")
         df.drop_duplicates(inplace=True)
-        
+
         # filters
-        tf_yesterday = df["dt"] >= logical_date.start_of('day').subtract(days=1).naive()
-        tf_today = df["dt"] < logical_date.start_of('day').naive()
+        tf_yesterday = df["dt"] >= logical_date.start_of("day").subtract(days=1).naive()
+        tf_today = df["dt"] < logical_date.start_of("day").naive()
 
         logging.info(" >> Printing filtered result")
-        filtered_data = df.loc[tf_yesterday & tf_today, ["dt", "city", "temp", "humidity"]]
+        filtered_data = df.loc[
+            tf_yesterday & tf_today, ["dt", "city", "temp", "humidity"]
+        ]
         # print(filtered_data)
-        
+
         return filtered_data
 
     except botocore.exceptions.ClientError:
@@ -75,35 +79,49 @@ def extract_yesterday_data(logical_date: pendulum.DateTime) -> DataFrame:
 @task()
 def create_report(data: DataFrame):
     logger.info(">> Creating a report")
-    
+
     jinja = get_jinja()
-    template = jinja.get_template('weather.tpl.j2')
-    
+    template = jinja.get_template("weather.tpl.j2")
+
     # from IPython import embed; embed()
-    ts = pendulum.from_timestamp(data.iloc[0]['dt'].timestamp())
-    city = data.iloc[0]['city']
-    
+    ts = pendulum.from_timestamp(data.iloc[0]["dt"].timestamp())
+    city = data.iloc[0]["city"]
+
     model = {
-        'city': city,
-        'date': ts.to_date_string(),
-        'max_temp': data['temp'].max(),
-        'min_temp': data['temp'].min(),
-        'avg_temp': data['temp'].mean(),
-        'timestamp': pendulum.now('utc').to_datetime_string(),
+        "city": city,
+        "date": ts.to_date_string(),
+        "max_temp": data["temp"].max(),
+        "min_temp": data["temp"].min(),
+        "avg_temp": data["temp"].mean(),
+        "timestamp": pendulum.now("utc").to_datetime_string(),
         # 'temp_unit': '°C'
     }
-    
+
     # render to temp file
     _, path = tempfile.mkstemp()
-    with open(path, 'w') as file:
+    with open(path, "w") as file:
         print(template.render(model), file=file)
-        
+
     # upload to s3/minio
     bucket = get_minio().Bucket(REPORTS_BUCKET)
-    bucket.upload_file(path, f'{city}.txt')
-    
+    bucket.upload_file(path, f"{city.lower()}.txt")
+
     # cleanup
     Path(path).unlink(True)
+
+
+@task
+def notify(logical_date: pendulum.DateTime):
+    yesterday = logical_date.start_of("day").subtract(days=1).naive()
+    
+    token = Variable.get("PUSHBULLET_TOKEN")
+
+    apprise = Apprise()
+    apprise.add(f"pbul://{token}")
+    apprise.notify(
+        title="Upozornenie!",
+        body=f"Nový report za deň {yesterday.to_date_string()} je pripravený.",
+    )
 
 
 @dag(
@@ -115,10 +133,10 @@ def create_report(data: DataFrame):
     tags=["weather", "devops", "dtit", "report"],
     catchup=False,
 )
-def main(hello: str = 'world'):
+def main(hello: str = "world"):
     # [ is_minio_alive ] -> [ extract_yesterday_data ] -> [ create_report ]
     extracted_data = is_minio_alive() >> extract_yesterday_data()
-    create_report(extracted_data)
+    create_report(extracted_data) >> notify()
 
 
 if __name__ == "__main__":

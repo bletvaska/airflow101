8d222b542ecef9c29e04a28f9f26c8e765812de2
mirek <mirek@cnl.sk>
2025-01-29 13:09:27 +0100

    paralelism for healthcheck

--- dags/weather_scraper.py
+++ dags/weather_scraper.py
@@ -105,7 +105,7 @@ def publish_data(line: str):
     tags=["weather", "devops", "dt"],
 )
 def main(query="kosice", units="metric"):
-    measurement = is_minio_alive() >> is_service_alive() >> scrape_data(query, units)
+    measurement = [ is_minio_alive(),  is_service_alive() ] >> scrape_data(query, units)
     valid_data = validate_data(measurement)
     entry = process_data(valid_data)
     publish_data(entry)

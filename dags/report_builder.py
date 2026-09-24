9cda635006aeaaaf5ff521d3065df0b1e9fa1d87
mirek <mirek@cnl.sk>
2026-09-24 13:55:46 +0200

    task ping

--- dags/report_builder.py
+++ dags/report_builder.py
@@ -1,20 +1,31 @@
+import logging
+
 from airflow.sdk import dag, task
 from pendulum import datetime
 
 from assets import WEATHER_DATA
 
 
+logger = logging.getLogger(__name__)
+
+
+@task
+def ping():
+    logger.info("-------------------------> PING")
+
+
 @dag(
-    'report_builder',
-    dag_display_name='Report Builder',
-    description='Builds report from last weather information.',
+    "report_builder",
+    dag_display_name="Report Builder",
+    description="Builds report from last weather information.",
     tags=["weather", "devops", "dt", "training"],
     start_date=datetime(2026, 9, 20),
     end_date=datetime(2026, 9, 27),
     catchup=False,
-    schedule=[ WEATHER_DATA ],
+    schedule=[WEATHER_DATA],
 )
 def main():
-    pass
+    ping()
+
 
 main()

f5a461048a79f1e922872e5844b3de5336086742
mirek <mirek@cnl.sk>
2026-09-25 13:23:51 +0200

    rozsireny midnight report

--- dags/midnight_report.py
+++ dags/midnight_report.py
@@ -1,16 +1,47 @@
 import logging
+from pathlib import Path
+import tempfile
 
 from airflow.sdk import dag, task
+from airflow.sdk.exceptions import AirflowFailException
 import pendulum
+from botocore.exceptions import ClientError
 
 from tasks import is_rustfs_alive
+from helpers import get_storage
+from constants import BUCKET_NAME, DATASET_FILE
 
 logger = logging.getLogger(__name__)
 
 
 @task(task_display_name="Create Report")
 def create_report():
-    pass
+    logger.info("Building Report")
+
+    # stiahni dataset
+    # (ak sa nepodarilo, tak skonci s chybou)
+    storage = get_storage()
+
+
+    bucket = storage.Bucket(BUCKET_NAME)
+
+    try:
+        path = Path(tempfile.mkstemp()[1])
+
+        try:
+            bucket.download_file(DATASET_FILE, path)
+        except ClientError as ex:
+            logger.warning("Dataset file not found. Nothing to do.")
+            raise AirflowFailException("Dataset file not found. Nothing to do.")
+
+        # magic
+        
+
+
+    finally:
+        # clean up
+        # (stiahnuty subor sa zmaze)
+        path.unlink(True)
 
 
 @dag(
